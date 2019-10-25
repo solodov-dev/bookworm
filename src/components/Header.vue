1d45793bda882a5ee05e364d66dfaf9cc3c2f350
Solodov <solodov-dev@gmail.com>
2019-10-25 19:56:09 +0300

    Header

---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app-bar id="bar" absolute color="white">
+    <v-img src="@/assets/logo.svg" max-width="180"></v-img>
+    <v-spacer></v-spacer>
+        <v-btn icon color="blue" @click="showSearch=!showSearch">
+          <v-icon>mdi-search-web</v-icon>
+        </v-btn>
+    <v-text-field v-if="showSearch"
+                  class="search-bar"
+                  v-model="searchTerm"
+                  label="Search"
+                  solo
+                  append-icon="mdi-book-search"
+                  @keyup.enter="searchBooks"
+                  @click:append="searchBooks"
+    ></v-text-field>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showSearch: false,
+      searchTerm: '',
+    };
+  },
+  methods: {
+    searchBooks() {
+      this.showSearch = false;
+      console.log(this.searchTerm);
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .search-bar {
+    z-index: 3;
+    position: absolute;
+    top: 80px;
+    right: 20%;
+    left: 20%;
+  }
+
+  @media screen and (max-width: 500px) {
+    .search-bar {
+      right: 5%;
+      left: 5%;
+    }
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img
+      class="empty-element"
+      alt="A worm eating books"
+      src="@/assets/books.svg"
+    />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+};
+</script>
+
+<style scoped>
+  .empty-element {
+    display: block;
+    width: 100px;
+    height: 100px;
+    margin: 250px auto;
+  }
+</style>
